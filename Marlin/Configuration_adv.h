bc15f16509c260abe084296de77f992a73b9d55e
David Farrell <davidpfarrell+github@gmail.com>
2023-08-20 18:39:16 -0700

    Revert disabled MIN_PROBE_EDGE_LEFT override value

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1451,7 +1451,7 @@
  * the probe to be unable to reach any points.
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
-  //#define MIN_PROBE_EDGE_LEFT 28
+  //#define MIN_PROBE_EDGE_LEFT MIN_PROBE_EDGE
   //#define MIN_PROBE_EDGE_RIGHT MIN_PROBE_EDGE
   //#define MIN_PROBE_EDGE_FRONT MIN_PROBE_EDGE
   //#define MIN_PROBE_EDGE_BACK MIN_PROBE_EDGE
